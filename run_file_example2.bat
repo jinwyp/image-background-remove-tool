da156bab294df284d4daee69216288af87f24bc3
JinWYP <jinwyp@gmail.com>
2021-02-06 04:13:20 +0800

    U-update

--- run_file_example2.bat
+++ run_file_example2.bat
@@ -1,2 +1,26 @@
 Rem The file will be saved under the original name with the extension .png along the path .\docs\imgs\output
-python3 main.py -i .\docs\imgs\input\1.jpg -o .\docs\imgs\output\ -m u2net
+python main.py -i .\docs\imgs\input\test1.jpg -o .\docs\imgs\output\ -m u2netp -prep bbd-fastrcnn -postp None
+
+python main.py -i .\docs\imgs\input\test2.jpg -o .\docs\imgs\output\ -m u2netp -prep bbd-fastrcnn -postp None  
+
+python main.py -i .\docs\imgs\input\test3.jpg -o .\docs\imgs\output\ -m u2netp -prep bbd-fastrcnn -postp None  
+
+python main.py -i .\docs\imgs\input\test4.jpg -o .\docs\imgs\output\ -m u2netp -prep bbd-fastrcnn -postp None  
+
+python main.py -i .\docs\imgs\input\test5.jpg -o .\docs\imgs\output\ -m u2netp -prep bbd-fastrcnn -postp None  
+
+python main.py -i .\docs\imgs\input\test6.jpg -o .\docs\imgs\output\ -m u2netp -prep bbd-fastrcnn -postp None  
+
+python main.py -i .\docs\imgs\input\test7.png -o .\docs\imgs\output\ -m u2netp -prep bbd-fastrcnn -postp None  
+
+python main.py -i .\docs\imgs\input\test8.png -o .\docs\imgs\output\ -m u2netp -prep bbd-fastrcnn -postp None  
+
+python main.py -i .\docs\imgs\input\test9.jpg -o .\docs\imgs\output\ -m u2netp -prep bbd-fastrcnn -postp None  
+
+python main.py -i .\docs\imgs\input\test10.png -o .\docs\imgs\output\ -m u2netp -prep bbd-fastrcnn -postp None  
+
+python main.py -i .\docs\imgs\input\test11.jpg -o .\docs\imgs\output\ -m u2netp -prep bbd-fastrcnn -postp None  
+
+
+
+
